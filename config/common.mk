d69a2af01a4d6124d6af52f4ec2b746179c9180c
Apu V <theapu@gmail.com>
2016-05-21 16:46:34 -0400

    UNOFFICIAL build type removed

--- config/common.mk
+++ config/common.mk
@@ -33,13 +33,13 @@ PRODUCT_BOOTANIMATION := vendor/cm/prebuilt/common/bootanimation/$(TARGET_BOOTAN
 endif
 endif
 
-ifdef CM_NIGHTLY
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.rommanager.developerid=cyanogenmodnightly
-else
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.rommanager.developerid=cyanogenmod
-endif
+#ifdef CM_NIGHTLY
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    ro.rommanager.developerid=cyanogenmodnightly
+#else
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    ro.rommanager.developerid=cyanogenmod
+#endif
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
@@ -232,44 +232,51 @@ PRODUCT_VERSION_MAINTENANCE = 0
 
 # Set CM_BUILDTYPE from the env RELEASE_TYPE, for jenkins compat
 
-ifndef CM_BUILDTYPE
-    ifdef RELEASE_TYPE
-        # Starting with "CM_" is optional
-        RELEASE_TYPE := $(shell echo $(RELEASE_TYPE) | sed -e 's|^CM_||g')
-        CM_BUILDTYPE := $(RELEASE_TYPE)
-    endif
-endif
+#ifndef CM_BUILDTYPE
+#    ifdef RELEASE_TYPE
+#        # Starting with "CM_" is optional
+#        RELEASE_TYPE := $(shell echo $(RELEASE_TYPE) | sed -e 's|^CM_||g')
+#        CM_BUILDTYPE := $(RELEASE_TYPE)
+#    endif
+#endif
 
 # Filter out random types, so it'll reset to UNOFFICIAL
-ifeq ($(filter RELEASE NIGHTLY SNAPSHOT EXPERIMENTAL,$(CM_BUILDTYPE)),)
-    CM_BUILDTYPE :=
-endif
+#ifeq ($(filter RELEASE NIGHTLY SNAPSHOT EXPERIMENTAL,$(CM_BUILDTYPE)),)
+#    CM_BUILDTYPE :=
+#endif
+
+#ifdef CM_BUILDTYPE
+#    ifneq ($(CM_BUILDTYPE), SNAPSHOT)
+#        ifdef CM_EXTRAVERSION
+#            # Force build type to EXPERIMENTAL
+#            CM_BUILDTYPE := EXPERIMENTAL
+#            # Remove leading dash from CM_EXTRAVERSION
+#            CM_EXTRAVERSION := $(shell echo $(CM_EXTRAVERSION) | sed 's/-//')
+#            # Add leading dash to CM_EXTRAVERSION
+#            CM_EXTRAVERSION := -$(CM_EXTRAVERSION)
+#        endif
+#    else
+#        ifndef CM_EXTRAVERSION
+#            # Force build type to EXPERIMENTAL, SNAPSHOT mandates a tag
+#            CM_BUILDTYPE := EXPERIMENTAL
+#        else
+#            # Remove leading dash from CM_EXTRAVERSION
+#            CM_EXTRAVERSION := $(shell echo $(CM_EXTRAVERSION) | sed 's/-//')
+#            # Add leading dash to CM_EXTRAVERSION
+#            CM_EXTRAVERSION := -$(CM_EXTRAVERSION)
+#        endif
+#    endif
+#else
+#    # If CM_BUILDTYPE is not defined, set to UNOFFICIAL
+#    CM_BUILDTYPE := UNOFFICIAL
+#    CM_EXTRAVERSION :=
+#endif
 
 ifdef CM_BUILDTYPE
-    ifneq ($(CM_BUILDTYPE), SNAPSHOT)
-        ifdef CM_EXTRAVERSION
-            # Force build type to EXPERIMENTAL
-            CM_BUILDTYPE := EXPERIMENTAL
-            # Remove leading dash from CM_EXTRAVERSION
-            CM_EXTRAVERSION := $(shell echo $(CM_EXTRAVERSION) | sed 's/-//')
-            # Add leading dash to CM_EXTRAVERSION
-            CM_EXTRAVERSION := -$(CM_EXTRAVERSION)
-        endif
-    else
-        ifndef CM_EXTRAVERSION
-            # Force build type to EXPERIMENTAL, SNAPSHOT mandates a tag
-            CM_BUILDTYPE := EXPERIMENTAL
-        else
-            # Remove leading dash from CM_EXTRAVERSION
-            CM_EXTRAVERSION := $(shell echo $(CM_EXTRAVERSION) | sed 's/-//')
-            # Add leading dash to CM_EXTRAVERSION
-            CM_EXTRAVERSION := -$(CM_EXTRAVERSION)
-        endif
-    endif
+        CM_BUILDTYPE := APUV
 else
-    # If CM_BUILDTYPE is not defined, set to UNOFFICIAL
-    CM_BUILDTYPE := UNOFFICIAL
-    CM_EXTRAVERSION :=
+	# If CM_BUILDTYPE is not defined, set to UNOFFICIAL
+	CM_BUILDTYPE := APUV
 endif
 
 ifeq ($(CM_BUILDTYPE), UNOFFICIAL)
